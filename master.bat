ffe857f6b3b573d640f6619d5e9f04fc425ad510
edsilv <e.silverton@gmail.com>
2015-04-24 15:01:50 +0100

    checkout branch first

--- master.bat
+++ master.bat
@@ -1,8 +1,3 @@
 call git checkout master
-<<<<<<< HEAD
-call git submodule foreach git pull origin master
-call git submodule foreach git checkout master
-=======
 call git submodule foreach git checkout master
 call git submodule foreach git pull origin master
->>>>>>> 41ef7d9... checkout branch first
