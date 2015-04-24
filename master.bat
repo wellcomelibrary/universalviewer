0c918793f971b825677781a1e883f9d891d462c5
edsilv <e.silverton@gmail.com>
2015-04-24 14:58:51 +0100

    merge

--- master.bat
+++ master.bat
@@ -1,3 +1,8 @@
 call git checkout master
+<<<<<<< HEAD
 call git submodule foreach git pull origin master
 call git submodule foreach git checkout master
+=======
+call git submodule foreach git checkout master
+call git submodule foreach git pull origin master
+>>>>>>> 41ef7d9... checkout branch first
